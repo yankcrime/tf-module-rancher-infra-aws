87c10f219b8e7f6e036179a5e078875807881809
Nick Jones <nick@dischord.org>
2020-06-11 16:43:37 +0100

    Force usage of ssh-agent

--- main.tf
+++ main.tf
@@ -2,14 +2,14 @@
 
 # Create Nodes
 resource "aws_instance" "node_master" {
-  count           = var.node_master_count
-  ami             = data.aws_ami.ubuntu.id
-  instance_type   = var.instance_type
-  key_name        = aws_key_pair.rancher_key_pair.key_name
+  count                       = var.node_master_count
+  ami                         = data.aws_ami.ubuntu.id
+  instance_type               = var.instance_type
+  key_name                    = aws_key_pair.rancher_key_pair.key_name
   vpc_security_group_ids      = [aws_security_group.rancher_nodes.id]
   subnet_id                   = element(tolist(data.aws_subnet_ids.available.ids), 0)
   associate_public_ip_address = true
-  user_data = local.node_master_cloudinit
+  user_data                   = local.node_master_cloudinit
   root_block_device {
     volume_type = "gp2"
     volume_size = "50"
@@ -20,10 +20,9 @@ resource "aws_instance" "node_master" {
     ]
 
     connection {
-      type        = "ssh"
-      host        = self.public_ip
-      user        = var.node_username
-      private_key = file(var.ssh_key_file)
+      type = "ssh"
+      host = self.public_ip
+      user = var.node_username
     }
   }
   tags = {
@@ -34,14 +33,14 @@ resource "aws_instance" "node_master" {
 }
 
 resource "aws_instance" "node_worker" {
-  count           = var.node_worker_count
-  ami             = data.aws_ami.ubuntu.id
-  instance_type   = var.instance_type
-  key_name        = aws_key_pair.rancher_key_pair.key_name
+  count                       = var.node_worker_count
+  ami                         = data.aws_ami.ubuntu.id
+  instance_type               = var.instance_type
+  key_name                    = aws_key_pair.rancher_key_pair.key_name
   vpc_security_group_ids      = [aws_security_group.rancher_nodes.id]
   subnet_id                   = element(tolist(data.aws_subnet_ids.available.ids), 0)
   associate_public_ip_address = true
-  user_data = local.node_worker_cloudinit
+  user_data                   = local.node_worker_cloudinit
   root_block_device {
     volume_type = "gp2"
     volume_size = "50"
@@ -52,10 +51,9 @@ resource "aws_instance" "node_worker" {
     ]
 
     connection {
-      type        = "ssh"
-      host        = self.public_ip
-      user        = var.node_username
-      private_key = file(var.ssh_key_file)
+      type = "ssh"
+      host = self.public_ip
+      user = var.node_username
     }
   }
   tags = {
@@ -66,14 +64,14 @@ resource "aws_instance" "node_worker" {
 }
 
 resource "aws_instance" "node_all" {
-  count           = var.node_all_count
-  ami             = data.aws_ami.ubuntu.id
-  instance_type   = var.instance_type
-  key_name        = aws_key_pair.rancher_key_pair.key_name
+  count                       = var.node_all_count
+  ami                         = data.aws_ami.ubuntu.id
+  instance_type               = var.instance_type
+  key_name                    = aws_key_pair.rancher_key_pair.key_name
   vpc_security_group_ids      = [aws_security_group.rancher_nodes.id]
   subnet_id                   = element(tolist(data.aws_subnet_ids.available.ids), 0)
   associate_public_ip_address = true
-  user_data = local.node_all_cloudinit
+  user_data                   = local.node_all_cloudinit
   root_block_device {
     volume_type = "gp2"
     volume_size = "50"
@@ -84,10 +82,9 @@ resource "aws_instance" "node_all" {
     ]
 
     connection {
-      type        = "ssh"
-      host        = self.public_ip
-      user        = var.node_username
-      private_key = file(var.ssh_key_file)
+      type = "ssh"
+      host = self.public_ip
+      user = var.node_username
     }
   }
   tags = {
--- output.tf
+++ output.tf
@@ -2,14 +2,13 @@
 
 output "rancher_nodes" {
   value = [
-  	for instance in flatten([[aws_instance.node_all], [aws_instance.node_master], [aws_instance.node_worker]]): {
-    
-    public_ip  = instance.public_ip
-    private_ip = instance.private_ip
-    hostname   = instance.id
-    user       = var.node_username
-    roles      = split(",", instance.tags.K8sRoles)
-    ssh_key    = file(var.ssh_key_file)
+    for instance in flatten([[aws_instance.node_all], [aws_instance.node_master], [aws_instance.node_worker]]) : {
+
+      public_ip  = instance.public_ip
+      private_ip = instance.private_ip
+      hostname   = instance.id
+      user       = var.node_username
+      roles      = split(",", instance.tags.K8sRoles)
     }
   ]
   sensitive = true
