76b7f40d0c18a722a32a2106df55f3c8f0d85199
Nick Jones <nick@dischord.org>
2020-08-27 15:50:48 +0100

    Use Ubuntu 20.04 instead of 18.04

--- data.tf
+++ data.tf
@@ -4,7 +4,7 @@ data "aws_ami" "ubuntu" {
   owners      = ["099720109477"] # Canonical
   filter {
     name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-amd64-server-*"]
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
   }
   filter {
     name   = "virtualization-type"
